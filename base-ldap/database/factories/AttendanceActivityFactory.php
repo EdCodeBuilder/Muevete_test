c36938a5bf0babb2630dbaf866ec4a99d5f7d4da
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2022-10-22 14:39:38 -0500

    Generacion de nuevo factory

---
+++ base-ldap/database/factories/AttendanceActivityFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AttendanceActivity;
+use Faker\Generator as Faker;
+
+$factory->define(AttendanceActivity::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
