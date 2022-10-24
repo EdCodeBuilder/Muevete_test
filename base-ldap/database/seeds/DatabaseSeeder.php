7bd2506d0ab4a3a12d9eabb08d3ecd45a5fbc441
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2022-10-24 12:30:10 -0500

    Modificacion en la semilla

--- base-ldap/database/seeds/DatabaseSeeder.php
+++ base-ldap/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // factory(App\Modules\CitizenPortal\src\Models\UsersAssistance::class,50)->create();
+        factory(App\Modules\CitizenPortal\src\Models\AttendanceActivity::class,30)->create();
     }
 }
