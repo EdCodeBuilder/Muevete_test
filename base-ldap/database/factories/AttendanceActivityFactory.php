647afc79571407c6d49cb2f45b7d15cf3141db40
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2022-10-24 12:28:03 -0500

    modificacion de datos para factory

--- base-ldap/database/factories/AttendanceActivityFactory.php
+++ base-ldap/database/factories/AttendanceActivityFactory.php
@@ -2,11 +2,23 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\AttendanceActivity;
+use App\Modules\CitizenPortal\src\Models\AttendanceActivity;
 use Faker\Generator as Faker;
 
 $factory->define(AttendanceActivity::class, function (Faker $faker) {
     return [
-        //
+        'id'            =>rand(23, 26),
+        'fecha'         =>$faker->date($format = 'Y-m-d'),
+        // 'date'       =>null,
+        'institucion'   =>$faker->word(),
+        'actividad'     =>$faker->sentence(),
+        'contenido'     =>$faker->paragraph(),
+        'hora_inicio'   =>$faker->dateTime(),
+        'hora_fin'      =>$faker->dateTime()
+        /*
+        'start_time'    =>null,
+        'end_time'      =>null,
+        'deleted_at'    =>null
+        */
     ];
 });
